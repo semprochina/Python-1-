8663460c17ecc7d11f33983e518297832f5c9b76
semprochina <sempro777@outlook.com>
2026-03-14 16:31:59 +0800

    Add files via upload

---
+++ 2508代码/1.单行注释.py
@@ -0,0 +1,6 @@
+# 龙仔是吴彦祖的形容词
+
+# import是导入操作
+# random是随机的模块  就是别人写好的代码
+import random
+
---
+++ 2508代码/6.个人信息输出.py
@@ -0,0 +1,4 @@
+name = input("请输入你的姓名:")
+age = input("请输入你的年龄:")
+hobby = input("请输入你的爱好:")
+print(f"我的姓名:{name} 我的年龄:{age} 我的爱好:{hobby}")
